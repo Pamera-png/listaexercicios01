c797ec1f76f8a2bf59f0de416832abda66e8599e
Pamera-png <pameranatali@gmail.com>
2021-11-24 15:47:23 -0300

    commit do exercício06

---
+++ exercicio06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("digite um número que corresponde ao dia da semana (1 a 7)");
+            int diaSemana = Convert.ToInt32(Console.ReadLine());
+            switch (diaSemana)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                    default:
+                    Console.WriteLine("dia da semana inválido");
+                    break;
+            }
+
+        }
+    }
+}
