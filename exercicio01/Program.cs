7afe7a69966c402537decaa63e645eae216bf632
Pamera-png <pameranatali@gmail.com>
2021-11-24 09:44:26 -0300

    meu primeiro commit

---
+++ exercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+Console.WriteLine("digite a primeira nota");
+String? textoNota01 = Console.ReadLine();
+Console.WriteLine("digite a segunda nota");
+String? textoNota02 = Console.ReadLine();
+double nota01 = Convert.ToDouble(textoNota01);
+double nota02 = Convert.ToDouble(textoNota02);
+double media = (nota01 + nota02) / 2;
+if (media == 10){
+    Console.WriteLine("aprovado com distinção");
+}
+else if (media >= 7){
+    Console.WriteLine("aprovado");
+}
+else{
+    Console.WriteLine("reprovado");
+}
+        }
+    }
+}
