52c272dfceee6f64d9cc38673d7b2ed0a155eaf3
Pamera-png <pameranatali@gmail.com>
2021-11-24 14:26:50 -0300

    commit do exercício 3

---
+++ exercicio03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio03{
+    public class Program    {
+
+        public static void Main(string[] args)        {
+Console.WriteLine("quanto custa o primeiro produto");
+String? textoproduto1 =  Console.ReadLine();
+Console.WriteLine("quanto custa o segundo produto");
+String? textoproduto2 =  Console.ReadLine();
+Console.WriteLine("quanto custa o terceiro produto");
+String? textoproduto3 =  Console.ReadLine();
+int produto1 = Convert.ToInt32(textoproduto1);
+int produto2 = Convert.ToInt32(textoproduto2);
+int produto3 = Convert.ToInt32(textoproduto3);
+int min = 0;
+if (produto1 < produto2 && produto1 < produto3){
+min = produto1;
+}
+    else if (produto2 < produto3){
+min = produto2;
+    }
+else{
+    min = produto3;
+}
+Console.WriteLine("o    produto de menor preço é o que custa" + min + "reais");
+
+}
+}
+}
