3e2ea3c1dfa4b768b31e58d4b42e4067def2e7e8
Pamera-png <pameranatali@gmail.com>
2021-11-26 16:00:02 -0300

    commit das ações do dia 26/11

---
+++ exercicio08/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio02{
+    public class Program    {
+
+        public static void Main(string[] args)        {
+Console.WriteLine("digite o primeiro lado");
+int lado1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite o segundo lado");
+int lado2 = Convert.ToInt32(Console.ReadLine());
+if (lado1 <  lado2 + lado3 && lado2 < lado1 lado1 + lado3) 
+}
+}
+}
