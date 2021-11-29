3949b7a1a0ec4a22836e7c8610ebd13f1cc29f33
Pamera-png <pameranatali@gmail.com>
2021-11-29 19:25:24 -0300

    commit do exercício 10

---
+++ exercicio10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio02{
+    public class Program    {
+
+        public static void Main(string[] args)        {
+Console.WriteLine("digite um número inteiro positivo e eu te direi se ele é par ou ímpar");
+int numero = Convert.ToInt32(Console.ReadLine());
+double resultado = numero % 2;
+if (resultado == 0){
+    Console.WriteLine("o número" + numero + "é par");
+}
+else{
+Console.WriteLine($"o número {numero} é ímpar");
+}
+
+}
+}
+}
