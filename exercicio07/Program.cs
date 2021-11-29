76c3e08db6b83d9fb03050180d1dd358de39c1de
Pamera-png <pameranatali@gmail.com>
2021-11-29 19:11:41 -0300

    commit do exercício 07

---
+++ exercicio07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio02{
+    public class Program    {
+
+        public static void Main(string[] args)        {
+Console.WriteLine("digite a primeira nota");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("digite a segunda nota");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+double media = (nota1 + nota2) / 2;
+Console.WriteLine("sua média é" + media);
+if (media <= 10 && media > 9){
+    Console.WriteLine("seu conceito é A. Você foi aprovado");
+}
+else if (media >= 7.5){
+ Console.WriteLine("seu conceito é B. Você foi aprovado");
+}
+else if (media >= 6.0){
+Console.WriteLine("seu conceito é C. Você foi aprovado");
+}
+else if (media >= 4.0){
+    Console.WriteLine("Seu conceito é D. Você foi reprovado");
+}
+else if (media <  4.0 && media >= 0){
+    Console.WriteLine("seu conceito é E. Você foi reprovado");
+}
+else{
+    Console.WriteLine("nota inválida");
+}
+
+        }
+}
+}
